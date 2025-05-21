14f5667d54e1ec634f58b7d9deb485e8b0644aff
sambody <samdanielsson000@gmail.com>
2025-05-21 20:37:34 +0200

    booom!

---
+++ CCWC/LinuxCommandWC.cs
@@ -0,0 +1,49 @@
+﻿namespace CCWC;
+
+public static class LinuxCommandWC
+{
+    private const string NoSuchCommand = "no such command";
+    private const string baseFilePath = "C:\\Users\\Sam\\RiderProjects\\CCWC\\";
+    private static string useCommand { get; set; }
+    private static string useFileName { get; set; }
+    
+    public static void Command(string command)
+    {
+       if (ValidCommand(command))
+       {
+           RunCommand();
+       }
+       else
+       {
+           Console.WriteLine(NoSuchCommand);
+       }
+    }
+
+    private static void RunCommand()
+    {
+        if (useCommand.Equals("-c"))
+        {
+            Byte[] fileBytes = File.ReadAllBytes(baseFilePath + useFileName);
+            Console.WriteLine(fileBytes.Length);
+        }
+        StreamReader reader = new StreamReader(baseFilePath + useFileName);
+        
+        
+    }
+
+    private static bool ValidCommand(string command)
+    {
+        string [] splitCommand  = command.Trim().Split(" ");
+        string commandName = splitCommand[0];
+        string fileName = splitCommand[1];
+
+        if (commandName != null && commandName.ToLower().Equals("-c") && fileName.ToLower().Equals("test.txt"))
+        {
+            useCommand = commandName.ToLower();
+            useFileName = fileName;
+            return true;
+        }
+        
+        return false;
+    }
+}
---
+++ CCWC/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CCWC;
+
+bool running = true;
+
+Console.WriteLine("type exit to exit");
+StreamReader reader = new StreamReader("C:\\Users\\Sam\\RiderProjects\\CCWC\\test.txt");
+string line =  reader.ReadLine();
+Console.WriteLine(line);
+while (running)
+{
+    string input = Console.ReadLine();
+    LinuxCommandWC.Command(input);
+    if (input != null && input.ToLower().Equals("exit"))
+    {
+        running = false;
+    }
+}
+
